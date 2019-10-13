1ec8acf1383ee06381ef0bfc2dd3ee3236d51baf
Stuart Smith <ssmith_1975@hotmail.com>
2019-10-13 15:05:29 -0700

    Adding code to create nuget package

--- Build/BuildDateTimeNuget.cmd
+++ Build/BuildDateTimeNuget.cmd
@@ -4,3 +4,11 @@ set script_path=%~dp0
 call %script_path%setup.cmd
 
 %msbuild% /p:Configuration=Release %script_path%CreateNugetPackage.msbuild
+
+if %errorlevel% neq 0 goto failed
+pause
+exit /b 0
+
+:failed
+pause
+exit /b 1
