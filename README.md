0c2f9075975e2c183932e63b4d1b80e66643744b
Stuart Smith <ssmith_1975@hotmail.com>
2019-10-16 06:42:03 -0700

    Updating Readme to have example code contained in it. Adding comments to Test Date Time funciton tests

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # WaterFrontSoftware.DateTime
-This is a .net standard library used for setting and retrieving the Datetime used in C#. It gives a level of abstraction from the Datetime function given in C#, so it is possible to change the datetime used by an application without actually changing the datetime of the server that is running the code. 
+This is a .net standard library used for setting and retrieving the Datetime used in C#. It gives a level of abstraction from the Datetime function given in C#, so it is possible to change the datetime used by an application without actually changing the datetime of the server that the code is running on. 
+
+Below is a very simple example of using the UTC version of this function
+
+             DateTimeTestsHelper.DateTimesAreWithIn2Seconds(currentDataTime, overRideDataTime);
+
+            //Sets the current date time to be Christmas 1900
+            WriteLine($"The UTC Date/Time before the using is {DateTime.UtcNow} ");
+            using (new DateTimeOverRideSetterUTC(Christmas1900))
+            {
+                WriteLine($"The UTC Date/Time with in the using statement is Christmase 1900 {DateTimeOverride.UtcNow} ");
+               
+            }
+
+            WriteLine($"The UTC Date/Time after the using statement is {DateTime.UtcNow} ");
+
+Output from this function is ...
+            
+            The UTC Date/Time before the using is 10/13/2019 10:48:12 PM 
+            The UTC Date/Time with in the using statement is Christmase 1900  12/25/1900 10:30:00 AM 
+            The UTC Date/Time after the using statement is 10/13/2019 10:48:12 PM 
+
+So if we wrap our test code in a using statement  and use the date time Overwrite vs the the Standard date time. We are able to override the datetime the code is using.
+
+
--- WaterFrontSoftware.DateTime.Tests/TestDateTimeFunctions.cs
+++ WaterFrontSoftware.DateTime.Tests/TestDateTimeFunctions.cs
@@ -19,7 +19,7 @@ namespace Waterfrontsoftware.DateTimeFunctions.Tests
             DateTime currentDataTime = DateTime.Now;
             DateTime overRideDataTime = DateTimeOverride.Now;
 
-            DateTimeTestsHelper.CheckDateTime(currentDataTime, overRideDataTime);
+            DateTimeTestsHelper.DateTimesAreWithIn2Seconds(currentDataTime, overRideDataTime);
 
             using (new DateTimeOverRideSetter(Christmas1900))
             {
@@ -35,14 +35,18 @@ namespace Waterfrontsoftware.DateTimeFunctions.Tests
             DateTime currentDataTime = DateTime.UtcNow;
             DateTime overRideDataTime = DateTimeOverride.UtcNow;
 
-            DateTimeTestsHelper.CheckDateTime(currentDataTime, overRideDataTime);
+            DateTimeTestsHelper.DateTimesAreWithIn2Seconds(currentDataTime, overRideDataTime);
 
+            //Sets the current date time to be Christmas 1900
+            WriteLine($"The UTC Date/Time before the using is {DateTime.UtcNow} ");
             using (new DateTimeOverRideSetterUTC(Christmas1900))
             {
+                WriteLine($"The UTC Date/Time with in the using statement is Christmase 1900  {DateTimeOverride.UtcNow} ");
                 DateShouldBeChristmas1900(DateTimeOverride.UtcNow, dateFormat);
             }
 
-            //Make sure the current data time is not that of Christmas
+            WriteLine($"The UTC Date/Time after the using statement is {DateTime.UtcNow} ");
+            //Once the using is complete the date time should be current date time
             var CurrentDate = DateTimeOverride.UtcNow;
             Assert.IsFalse(CurrentDate.ToString(dateFormat) == Christmas1900.ToString(dateFormat));
         }
@@ -68,16 +72,23 @@ namespace Waterfrontsoftware.DateTimeFunctions.Tests
         }
     }
 
-
     [ExcludeFromCodeCoverage]
     internal static class DateTimeTestsHelper
     {
-        static internal void CheckDateTime(DateTime currentDataTime, DateTime overRideDataTime)
+        /// <summary>
+        /// Check to make sure the difference of two date and time objects passed in is no greater that 2 seconds
+        /// </summary>
+        /// <param name="currentDataTime"></param>
+        /// <param name="overRideDataTime"></param>
+        static internal void DateTimesAreWithIn2Seconds(DateTime currentDataTime, DateTime overRideDataTime)
         {
             bool checkTime = (Abs((currentDataTime - overRideDataTime).TotalSeconds) < 2) || (Abs((overRideDataTime - currentDataTime).TotalSeconds) < 2);
             Assert.IsTrue(checkTime, "The current datetime and the one in returned by the date Time Over Ride should be no less that 2 seconds apart");
         }
 
+        /// <summary>
+        /// Creates a Date time obects set to Christmas 1900
+        /// </summary>
         static internal DateTime Christmas1900
         {
             get
@@ -86,6 +97,9 @@ namespace Waterfrontsoftware.DateTimeFunctions.Tests
             }
         }
 
+        /// <summary>
+        /// Creates a Date time obects set to Christmas 1800
+        /// </summary>
         static internal DateTime Christmas1800
         {
             get
@@ -94,9 +108,18 @@ namespace Waterfrontsoftware.DateTimeFunctions.Tests
             }
         }
 
+        /// <summary>
+        /// Check to makes sure the date time is christmas 1900
+        /// </summary>
         static internal void DateShouldBeChristmas1900(DateTime currentDateIs, string dateFormat)
         {
             Assert.IsTrue(currentDateIs.ToString(dateFormat) == Christmas1900.ToString(dateFormat), $"The current date should be Christmas 1900 but it is { currentDateIs.ToString(dateFormat)}");
+        }
+
+        static internal void WriteLine(string lineToWrite)
+        {
+            Console.WriteLine(lineToWrite);
+            System.Diagnostics.Debug.WriteLine(lineToWrite);
         }
     }
 }
